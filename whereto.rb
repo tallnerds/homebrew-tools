0378370b017152c62a07dd38cb576d68f442d1b5
Matt McFarland <783024+vanetix@users.noreply.github.com>
2021-11-04 07:21:03 -0600

    chore: update whereto sha256sum

--- whereto.rb
+++ whereto.rb
@@ -1,9 +1,9 @@
 class Whereto < Formula
+  version "1.0.0"
   desc "A CLI tool for tracking down redirects"
   homepage "https://github.com/tallnerds/whereto"
-  url "https://github.com/tallnerds/whereto/releases/download/1.0.0/whereto_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "<generate>"
+  url "https://github.com/tallnerds/whereto/releases/download/#{version}/whereto_#{version}_darwin_amd64.tar.gz"
+  sha256 "cc1ed88c39e27d7cc916c8d89ab49a0bab8f42c08a2d1ba3c36def078d69ca56"
 
   def install
     bin.install "whereto"
